69a006774d9b7f9d2d0f5fb1a404257282572862
NickProkhorov <nickky87@yandex.ru>
2023-04-06 23:22:45 +0300

    исправил .env и адрес БД в app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/router');
 const errorHandler = require('./middlewares/error-handler');
 
-const { PORT = 3000, DB_ADDRESS } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:3000' }));
@@ -24,7 +24,7 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-mongoose.connect(DB_ADDRESS);
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.get('/', (req, res) => {
   res.send('hello world!');
